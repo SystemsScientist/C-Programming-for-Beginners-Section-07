8a1ea8bc41c8fad051617f8935a90501baedef28
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-21 06:49:55 -0600

    Adding If States files and directories

---
+++ If-Statements/main.c
@@ -0,0 +1,26 @@
+/*
+    Author: Matt Johnson
+    Subject: if statements
+    Purpose: test if integer is even or odd
+    Date: 01/21/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int number_to_test, remainder;
+
+    printf("Enter your number to be tested: ");
+    scanf("%i", &number_to_test);
+
+    remainder = number_to_test % 2;
+
+    if (remainder == 0)
+        printf("The number is even.\n");
+    else
+        printf("The number is odd.\n");
+
+    return 0;
+}
